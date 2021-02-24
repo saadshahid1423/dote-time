73a8f6dfce3fa42b2f9eca1bc888b419108c2163
saadshahid1423 <saadshahid1423@gmail.com>
2021-02-24 17:58:44 +0500

    finaltouchup

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router,Route,} from "react-router-dom";
-import First from '../screens/first/first';
+import Note from '../screens/note/note';
 import Second from '../screens/second/second';
 
 class AppRouter extends React.Component{
     render(){
         return(
             <Router>
-                <Route exact path="/" component={First} />
+                <Route exact path="/" component={Note} />
                 <Route path="/second" component={Second} />
             </Router>
         )
---
+++ src/screens/note/note.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import App from '../../App.css';
+
+class Note extends React.Component{
+    Save= ()=>{
+        prompt("Enter File Name")
+    }
+    render(){
+        return(
+            <div className="firstpage">
+                <div className="textinputdiv">
+                <textarea defaultValue="My First TransNote" className="headdraft" type="text" required/>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Hello, it is your first TransNote" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="This document is created based on your intrests. Don't miss trying the following features, and feel free to make an edits." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Try them now!" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Tab text to edit the note" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Drag node to adjust level" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="It's time to know that you can do with Transo" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="5R Note Taking" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Note record" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Colors: Inspire creativity and improve accuracy" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Comprehension: Be able to clearly distinguish between what you understand and what you don't understant" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Sort out clues" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Timed review" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Meeting Note" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Meeting overview" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Meeting times: 3pm, November 1,2019" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Meeting place: Meeting room 11" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Theme: Sales progress" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Meeting agenda" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Update and announce last week's sales data" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="To-do List" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="October 20, 2019" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="To do" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Book an airline ticket" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Doing" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Brainstorming" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Idea record" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="@Anne" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Increase the dimensions of data collection" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Follow-up to do" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Organize all the brainstorming ideas of the participants @Justin" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Assess the possibilities of all ideas @James" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Schedule" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="5:00-12:00" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="6:30-6:40 Get up" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="7:00-7:20 Breakfast" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="7:30-8:00 REcite fench words" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="12:00-14:00" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Reading Notes" className="headdraft2" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="'The Minto Pyramid Principle' Barbara Minto" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Part 1: Writing logic" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Why should we use the Pyramid principle" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="What is the pyramid structure" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="Thinking logic" className="headdraft1" type="text" required/>
+                </div>
+                <div className="inputbullet">
+                <li style={{color:"white"}}></li><textarea defaultValue="..." className="headdraft1" type="text" required/>
+                </div>
+                </div>
+                <div>
+                    <center>
+                <button onClick={this.Save} className="buttsave">Save</button>
+                </center>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Note;
